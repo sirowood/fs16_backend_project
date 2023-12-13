1f1abf3a87128940e92d692c9cbdfd25873249c8
Xuefeng Wu <sirowood@gmail.com>
2023-12-13 14:58:24 +0200

    Service - UserDTO

---
+++ Shopify.Service/src/DTO/UserDTO.cs
@@ -0,0 +1,28 @@
+using Shopify.Core.src.Entity;
+
+namespace Shopify.Service.src.DTO;
+
+public class UserReadDTO : BaseEntity
+{
+  public Role Role { get; set; }
+  public required string FirstName { get; set; }
+  public required string LastName { get; set; }
+  public required string Email { get; set; }
+  public required IEnumerable<AddressReadDTO> Addresses { get; set; }
+}
+
+public class UserCreateDTO
+{
+  public Role Role { get; set; } = Role.Customer;
+  public required string FirstName { get; set; }
+  public required string LastName { get; set; }
+  public required string Email { get; set; }
+  public required string Password { get; set; }
+}
+
+public class UserUpdateDTO
+{
+  public required string FirstName { get; set; }
+  public required string LastName { get; set; }
+  public required string Email { get; set; }
+}
