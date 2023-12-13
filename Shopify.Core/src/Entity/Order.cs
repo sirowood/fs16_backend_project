6fb701b76124871a4b82214fb4059a63c05807ba
Xuefeng Wu <sirowood@gmail.com>
2023-12-13 14:07:17 +0200

    bug fix - CustomerId -> UserId

--- Shopify.Core/src/Entity/Order.cs
+++ Shopify.Core/src/Entity/Order.cs
@@ -4,8 +4,7 @@ namespace Shopify.Core.src.Entity;
 
 public class Order : BaseEntity
 {
-  public required Guid CustomerId { get; set; }
-
+  public required Guid UserId { get; set; }
   public required Guid AddressId { get; set; }
   public required Status Status { get; set; } = Status.Unpaid;
   public required DateTime OrderTime { get; set; } = DateTime.Now;
