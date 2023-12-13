ea6d81780f5ca042f2f7add0e1ed83d1c2865d83
Xuefeng Wu <sirowood@gmail.com>
2023-12-13 16:02:06 +0200

    Service - UserService - CreateOne

---
+++ Shopify.Service/src/Service/UserService.cs
@@ -0,0 +1,51 @@
+using AutoMapper;
+using Shopify.Core.src.Abstraction;
+using Shopify.Core.src.Entity;
+using Shopify.Core.src.Shared;
+using Shopify.Service.src.Abstraction;
+using Shopify.Service.src.DTO;
+
+namespace Shopify.Service.src.Service;
+
+public class UserService : IUserService
+{
+  private readonly IUserRepo _userRepo;
+  private readonly IMapper _mapper;
+
+  public UserService(IUserRepo userRepo, IMapper mapper)
+  {
+    _userRepo = userRepo;
+    _mapper = mapper;
+  }
+
+  public UserReadDTO CreateOne(UserCreateDTO createDTO)
+  {
+    var result = _userRepo.CreateOne(_mapper.Map<UserCreateDTO, User>(createDTO));
+    return _mapper.Map<User, UserReadDTO>(result);
+  }
+
+  public bool DeleteOne(Guid id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public IEnumerable<UserReadDTO> GetAll(GetAllOptions options)
+  {
+    throw new NotImplementedException();
+  }
+
+  public UserReadDTO GetByEmail(string email)
+  {
+    throw new NotImplementedException();
+  }
+
+  public UserReadDTO GetById(Guid id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public UserReadDTO UpdateOne(UserUpdateDTO updateDTO)
+  {
+    throw new NotImplementedException();
+  }
+}
