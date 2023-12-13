be92d92ed4f0bf70f7e503004894c033a942c835
Xuefeng Wu <sirowood@gmail.com>
2023-12-13 14:52:46 +0200

    Service - AddressDTO

---
+++ Shopify.Service/src/DTO/AddressDTO.cs
@@ -0,0 +1,28 @@
+using Shopify.Core.src.Entity;
+
+namespace Shopify.Service.src.DTO;
+
+public class AddressReadDTO : BaseEntity
+{
+  public required string Street { get; set; }
+  public required string PostCode { get; set; }
+  public required string City { get; set; }
+  public required string Country { get; set; }
+}
+
+public class AddressCreateDTO
+{
+  public required string Street { get; set; }
+  public required string PostCode { get; set; }
+  public required string City { get; set; }
+  public required string Country { get; set; }
+  public required Guid UserId { get; set; }
+}
+
+public class AddressUpdateDTO
+{
+  public required string Street { get; set; }
+  public required string PostCode { get; set; }
+  public required string City { get; set; }
+  public required string Country { get; set; }
+}
