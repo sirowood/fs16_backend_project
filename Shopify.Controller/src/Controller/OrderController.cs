278ed20a4fbff30c442d42a77fbbb54fb988221a
Xuefeng Wu <sirowood@gmail.com>
2024-01-08 14:36:21 +0200

    Refactor - remove unused endpoint

--- Shopify.Controller/src/Controller/OrderController.cs
+++ Shopify.Controller/src/Controller/OrderController.cs
@@ -106,31 +106,6 @@ public class OrderController : BaseController<Order, OrderReadDTO, OrderCreateDT
   }
 
   [Authorize(Roles = "Customer")]
-  [HttpPatch("{id:guid}/pay")]
-  public async Task<ActionResult<bool>> PayOrderAsync([FromRoute] Guid id)
-  {
-    var order = await _service.GetByIdAsync(id)
-      ?? throw CustomException.NotFound("Order not found.");
-
-    if (order.Status != Status.Unpaid)
-    {
-      throw CustomException.NotAllowed("Only unpaid order can be paid.");
-    }
-
-    var authorizationResult = await _authorizationService
-      .AuthorizeAsync(User, order, "OrderOwner");
-
-    if (!authorizationResult.Succeeded)
-    {
-      throw CustomException.NotAllowed("You're not the order owner.");
-    }
-
-    await base.UpdateOneAsync(id, new OrderUpdateDTO { Status = Status.Paid });
-
-    return Ok(true);
-  }
-
-  [Authorize(Roles = "Customer")]
   [HttpPatch("{id:guid}/cancel")]
   public async Task<ActionResult<bool>> CancelOrderAsync([FromRoute] Guid id)
   {
